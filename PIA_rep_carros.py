c7d25ea544b34585e5e97ad65e0f0130349f8be2
Arkham Tovar <tovararkham@gmail.com>
2023-10-25 01:08:12 -0600

    primera prueba de pantalla

--- PIA_rep_carros.py
+++ PIA_rep_carros.py
@@ -1,3 +1,12 @@
 from tkinter import ttk
 from tkinter import *
 
+ventana_bienvenida=Tk()
+ventana_bienvenida.geometry("800x600")
+
+
+img_logo=PhotoImage(file="logo.png")
+etiqueta=Label(ventana_bienvenida, text="Reparacion de coches", image=img_logo)
+etiqueta.pack()
+
+ventana_bienvenida.mainloop()
