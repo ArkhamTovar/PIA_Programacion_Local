28dc05379533e9bb540e654b91bc78c5effd35eb
Arkham Tovar <tovararkham@gmail.com>
2023-10-27 23:36:51 -0600

    ventanas y menu

--- PIA_rep_carros.py
+++ PIA_rep_carros.py
@@ -1,12 +1,29 @@
 from tkinter import ttk
 from tkinter import *
+from PIL import ImageTk, Image
 
-ventana_bienvenida=Tk()
-ventana_bienvenida.geometry("800x600")
+def ventanaCarro():
+    frameCarro=Tk()
+    frameCarro.geometry('800x600')
+
+def ventanaCliente():
+    frameCliente=Tk()
+    frameCliente.geometry('800x800')
 
 
-img_logo=PhotoImage(file="logo.png")
-etiqueta=Label(ventana_bienvenida, text="Reparacion de coches", image=img_logo)
-etiqueta.pack()
 
-ventana_bienvenida.mainloop()
+ventanaBienvenida=Tk()
+ventanaBienvenida.title('Inicio')
+ventanaBienvenida.geometry('400x400')
+ventanaBienvenida.config(bg='blue')
+
+frameBienvenida=LabelFrame(text="Bienvenido al sistema", font=('arial',12), background='white')
+frameBienvenida.pack(fill='both', expand='yes',padx=20,pady=15)
+
+btnRegistroCarro=Button(ventanaBienvenida,text="registrar coche", width=12,height=9,command=ventanaCarro)
+btnRegistroCarro.place(x=50,y=40)
+
+btnRegistroCliente=Button(ventanaBienvenida,text="registrar cliente", width=12,height=9, command=ventanaCliente)
+btnRegistroCliente.place(x=200,y=40)
+    
+ventanaBienvenida.mainloop()
